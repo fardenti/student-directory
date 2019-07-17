06504d8b06244ffcc1a674cc91ef928a0f3305d0
farnaz ardenti <farnazardenti@gmail.com>
2019-07-17 14:50:00 +0100

    2.students whose name begins with a

--- directory.rb
+++ directory.rb
@@ -24,7 +24,7 @@ end
 def print(students)
   students.each_with_index do |student, index|
     if student[:name].start_with? "a"
-      puts "#{index}.#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
