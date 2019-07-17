1c45e85ba583b48c4fc460b5295d2bbcb7e77df1
farnaz ardenti <farnazardenti@gmail.com>
2019-07-17 14:52:16 +0100

    3. students whose name is shorter than 12 characters.

--- directory.rb
+++ directory.rb
@@ -23,7 +23,7 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student, index|
-    if student[:name].start_with? "a"
+    if student[:name].length < 12
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
