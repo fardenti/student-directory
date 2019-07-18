d16d80c7e440f95768fc3636dc76264c178a7d7e
farnaz ardenti <farnazardenti@gmail.com>
2019-07-18 14:31:21 +0100

    Exercise 8 - part 6

--- directory.rb
+++ directory.rb
@@ -1,17 +1,30 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the student"
+  name = gets.chomp
+  puts "Please enter the age of the student"
+  age = gets.chomp
+  puts "Please enter the country the student is from"
+  country = gets.chomp
+  puts "Please enter the gender of the student"
+  gender = gets.chomp
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: "november", age: age, country: country, gender: gender}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Please enter the names of the student"
     name = gets.chomp
+    puts "Please enter the age of the student"
+    age = gets.chomp
+    puts "Please enter the country the student is from"
+    country = gets.chomp
+    puts "Please enter the gender of the student"
+    gender = gets.chomp
+    puts "To finish, just hit return twice"
   end
   # return the array of students
   students
@@ -24,7 +37,7 @@ end
 def print(students)
   while true do
     students.each_with_index do |student, index|
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index + 1}. Name:#{student[:name].center(5)}, From: (#{student[:cohort].center(5)} cohort), Age: #{student[:age].center(5)}, Country: #{student[:country].center(5)}, Gender: #{student[:gender].center(5)}"
     end
     break
   end
